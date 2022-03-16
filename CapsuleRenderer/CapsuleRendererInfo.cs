b641963c51ae2382a9d0ca8a34482c4f553d766e
秋水 <1123993881@qq.com>
2022-03-16 10:32:57 +0800

    Add project files.

---
+++ CapsuleRenderer/CapsuleRendererInfo.cs
@@ -0,0 +1,28 @@
+﻿using Grasshopper;
+using Grasshopper.Kernel;
+using System;
+using System.Drawing;
+
+namespace CapsuleRenderer
+{
+    public class CapsuleRendererInfo : GH_AssemblyInfo
+    {
+        public override string Name => "Capsule Renderer";
+
+        //Return a 24x24 pixel bitmap to represent this GHA library.
+        public override Bitmap Icon => null;
+
+        //Return a short string describing the purpose of this GHA library.
+        public override string Description => "Advanced options for capsule rendering.";
+
+        public override Guid Id => new Guid("46D4702E-3C86-449A-ACAE-7348E5A9C098");
+
+        //Return a string identifying you or your company.
+        public override string AuthorName => "秋水";
+
+        //Return a string representing your preferred contact details.
+        public override string AuthorContact => "1123993881@qq.com";
+
+        public override string Version => "0.9.0";
+    }
+}
