9a4fe6f00d8cc6e4552cd8be9c759c89d4c51619
秋水 <1123993881@qq.com>
2024-06-25 16:52:48 +0800

    fix: opcodes fix.

--- CapsuleRenderer/CapsuleRendererInfo.cs
+++ CapsuleRenderer/CapsuleRendererInfo.cs
@@ -1,6 +1,7 @@
 ﻿using Grasshopper.GUI;
 using Grasshopper.Kernel;
 using HarmonyLib;
+using SimpleGrasshopper.Util;
 using System;
 using System.Drawing;
 
@@ -11,7 +12,7 @@ public class CapsuleRendererInfo : GH_AssemblyInfo
     public override string Name => "Capsule Renderer";
 
     //Return a 24x24 pixel bitmap to represent this GHA library.
-    public override Bitmap Icon => Properties.Resources.CapsuleRendererIcon_24;
+    public override Bitmap Icon => typeof(CapsuleRendererInfo).Assembly.GetBitmap("CapsuleRendererIcon_24.png");
 
     //Return a short string describing the purpose of this GHA library.
     public override string Description => "Advanced options for capsule rendering. Icon was made by ZCS.";
--- CapsuleRenderer/Patch/ComponentAttributePatch.cs
+++ CapsuleRenderer/Patch/ComponentAttributePatch.cs
@@ -1,24 +1,23 @@
 ﻿using Grasshopper.GUI.Canvas;
 using Grasshopper.Kernel.Attributes;
 using HarmonyLib;
-using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using System.Reflection.Emit;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CapsuleRenderer.Patch;
 
 [HarmonyPatch(typeof(GH_ComponentAttributes))]
 internal class ComponentAttributePatch
 {
-    [HarmonyPatch("RenderComponentCapsule", typeof(GH_Canvas), typeof(Graphics), typeof(bool), typeof(bool), typeof(bool), typeof(bool), typeof(bool), typeof(bool))]
+    [HarmonyPatch("RenderComponentCapsule", typeof(GH_Canvas), typeof(Graphics), 
+        typeof(bool), typeof(bool), typeof(bool), typeof(bool), typeof(bool), typeof(bool))]
     static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
     {
         var result = new List<CodeInstruction>();
         bool find = !Data.FixJaggedEdges;
+
         foreach (var instruction in instructions)
         {
             result.Add(instruction);
@@ -28,12 +27,12 @@ internal class ComponentAttributePatch
             find = true;
 
             result.Add(new(OpCodes.Ldloc_2));
-            result.Add(new(OpCodes.Ldarga_S, 4));
+            result.Add(new(OpCodes.Ldarg_S, 4));
             result.Add(new(OpCodes.And));
             result.Add(new(OpCodes.Stloc_2));
 
             result.Add(new(OpCodes.Ldloc_3));
-            result.Add(new(OpCodes.Ldarga_S, 4));
+            result.Add(new(OpCodes.Ldarg_S, 4));
             result.Add(new(OpCodes.And));
             result.Add(new(OpCodes.Stloc_3));
         }
