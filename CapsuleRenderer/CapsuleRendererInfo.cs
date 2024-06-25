da7d811c9d9073fdd266cf1e30ab21feb0ff49ad
秋水 <1123993881@qq.com>
2024-06-25 16:59:50 +0800

    chore: 0.9.4 released.

--- CapsuleRenderer/CapsuleRendererInfo.cs
+++ CapsuleRenderer/CapsuleRendererInfo.cs
@@ -25,7 +25,7 @@ public class CapsuleRendererInfo : GH_AssemblyInfo
     //Return a string representing your preferred contact details.
     public override string AuthorContact => "1123993881@qq.com";
 
-    public override string Version => "0.9.3";
+    public override string Version => "0.9.4";
 }
 
 partial class SimpleAssemblyPriority
